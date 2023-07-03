a64e5e128f6c862deb1489929528adf670748314
sainamercy <sainamercy@gmail.com>
2023-07-03 15:55:35 +0300

    project schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import {  deskTool } from "sanity/desk";
+import project from "./sanity/schemas/project-schemas";
 
 const config = defineConfig({
     projectId: "plt5qdnx",
@@ -7,7 +8,10 @@ const config = defineConfig({
     title: "My Personal Website",
     apiVersion: "2023-05-31",
     basePath: "/admin",
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: {
+        types: [project]
+    }
 });
 
 
