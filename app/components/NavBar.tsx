c1a96e51f4791444d095d6b4a055528801caff90
sainamercy <sainamercy@gmail.com>
2023-07-04 21:38:46 +0300

    typewriter animation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <nav className="h-16 flex justify-between px-80 items-center text-white absolute top-0 w-full">
       <Link href="/" className="text-4xl font-semibold">
-        Mercy<span className="text-lime-500">.</span>
+        Mercy Saina<span className="text-lime-500 text-6xl">.</span>
       </Link>
       <ul className="flex items-center justify-evenly h-full w-2/5">
         <li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Home() {
   return (
@@ -14,11 +16,24 @@ export default function Home() {
           height={600}
           className="absolute bottom-0 right-80"
         />
-        <div className="h-screen bg-black w-2/3 flex flex-col justify-center items-center text-white gap-4">
-          <h1 className="text-[6rem]">Hi I&apos;m Mercy</h1>
-          <p className="text-4xl p-6 bg-white border-4 border-lime-500 text-black">
-            A passionate software developer
-          </p>
+        <div className="h-screen bg-black w-2/3">
+          <div className="flex flex-col justify-center h-full text-white gap-4 w-1/2 mx-auto">
+            <h1 className="text-[6rem]">Hi I&apos;m Mercy</h1>
+            <p className="text-4xl p-4 bg-amber-100 border-4 border-lime-500 text-black max-w-fit">
+              I&apos;M A{" "}
+              <span className="text-gray-800">
+                <Typewriter
+                  words={["WEB DEVELOPER", "DESIGNER", "PROGRAMMER"]}
+                  loop={false}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={200}
+                  deleteSpeed={100}
+                  delaySpeed={1000}
+                />
+              </span>
+            </p>
+          </div>
         </div>
         <div className="w-1/3 h-screen bg-lime-500"></div>
       </div>
