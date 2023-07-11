b63f35b26049fa38333c9081e6a38d3ea2083d58
sainamercy <sainamercy@gmail.com>
2023-07-11 13:49:00 +0300

    framer-motion set up

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,20 +3,38 @@ import Image from "next/image";
 function About() {
   return (
     <section
-      className="h-screen flex items-center justify-between w-full"
+      className="h-screen flex flex-col items-center justify-center"
       id="about"
     >
-      <div className="">
-        <Image src="/heropage.png" alt="about" width={500} height={500} />
-      </div>
-      <div>
-        <h2>About</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum eos
-          nisi accusamus perspiciatis in dolore aperiam sint ratione velit
-          assumenda fuga repellendus, dolor optio explicabo magnam id
-          necessitatibus a. Aperiam!
-        </p>
+      <h3 className="text-4xl font-semibold mt-10 text-gray-800">
+        Learn about me
+      </h3>
+      <div className="flex items-center justify-center w-3/4 mx-auto gap-10">
+        <div className="bg-black">
+          <Image src="/aboutPage.png" alt="about" width={500} height={500} />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-10 w-full md:w-1/2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+            eos nisi accusamus perspiciatis in dolore aperiam sint ratione velit
+            assumenda fuga repellendus, dolor optio explicabo magnam id
+            necessitatibus a. Aperiam!
+          </p>
+          <div>
+            <h3>My skills</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Next.js</li>
+              <li>TypeScript</li>
+              <li>TailwindCSS</li>
+              <li>Redux</li>
+              <li>Node.js</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
--- app/components/SideSkills.tsx
+++ app/components/SideSkills.tsx
@@ -5,7 +5,7 @@ function SideSkills() {
   return (
     <div className="flex flex-col items-center justify-center">
       <h3 className="transform: rotate-90 flex items-center gap-2 mb-16 font-semibold">
-        MY EXPERTISE <FaMinus className="text-4xl text-white" />
+        MY EXPERTISE <FaMinus className="text-5xl text-white" />
       </h3>
       <div className="flex flex-col items-center justify-center gap-4">
         <div className="flex items-center justify-center w-12 h-12 bg-white rounded-full">
